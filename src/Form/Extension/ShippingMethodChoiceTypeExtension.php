bd3516c124254cac1e9d4e56da658714512bf31c
Joachim Løvgaard <joachim@loevgaard.dk>
2019-01-10 13:57:31 +0100

    fixed error in ajax call url

--- src/Form/Extension/ShippingMethodChoiceTypeExtension.php
+++ src/Form/Extension/ShippingMethodChoiceTypeExtension.php
@@ -65,7 +65,7 @@ final class ShippingMethodChoiceTypeExtension extends AbstractTypeExtension
 
                 return [
                         'data-pickup-point-provider' => $provider->getCode(),
-                        'data-pickup-point-provider-url' => $this->router->generate('setono_sylius_pickup_point_shop_ajax_find_pickup_points', ['provider' => $provider->getCode()]),
+                        'data-pickup-point-provider-url' => $this->router->generate('setono_sylius_pickup_point_shop_ajax_find_pickup_points', ['providerCode' => $provider->getCode()]),
                         'data-csrf-token' => $this->csrfTokenManager->getToken((string) $this->cartContext->getCart()->getId()),
                     ] + $defaultAttr;
             }
