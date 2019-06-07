a5354e87b86cf5fa1658a7080381d5ebedbde428
Igor Mukhin <igor.mukhin@gmail.com>
2019-06-07 12:04:08 +0300

    Added: Another action to retrieve PickupPoint by ID (full_id)

---
+++ src/Controller/Action/PickupPointAction.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusPickupPointPlugin\Controller\Action;
+
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\View\ViewHandlerInterface;
+use Setono\SyliusPickupPointPlugin\Model\PickupPointInterface;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+final class PickupPointAction
+{
+    /**
+     * @var ViewHandlerInterface
+     */
+    private $viewHandler;
+
+    /**
+     * @var DataTransformerInterface
+     */
+    private $pickupPointToIdentifierTransformer;
+
+    public function __construct(
+        ViewHandlerInterface $viewHandler,
+        DataTransformerInterface $pickupPointToIdentifierTransformer
+    ) {
+        $this->viewHandler = $viewHandler;
+        $this->pickupPointToIdentifierTransformer = $pickupPointToIdentifierTransformer;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        $pickupPointId = $request->get('pickupPointId');
+        if (!$pickupPointId) {
+            throw new NotFoundHttpException();
+        }
+
+        /** @var PickupPointInterface $pickupPoint */
+        $pickupPoint = $this->pickupPointToIdentifierTransformer->reverseTransform($pickupPointId);
+        if (!$pickupPoint instanceof PickupPointInterface) {
+            throw new NotFoundHttpException();
+        }
+
+        $view = View::create($pickupPoint);
+        $view->getContext()->addGroup('Detailed');
+        return $this->viewHandler->handle($view);
+    }
+}
