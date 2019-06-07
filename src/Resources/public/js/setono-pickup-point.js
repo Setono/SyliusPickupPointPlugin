c79aae18aeafcea862d3d2b510b7215544f80613
Igor Mukhin <igor.mukhin@gmail.com>
2019-06-07 17:35:20 +0300

    Removed: Current value append as dropdown should be empty on shipping method change

--- src/Resources/public/js/setono-pickup-point.js
+++ src/Resources/public/js/setono-pickup-point.js
@@ -28,7 +28,6 @@
         const element = $(el);
         const choiceName = element.data('choice-name');
         const choiceValue = element.data('choice-value');
-        const autocompleteValue = element.find('input.autocomplete').val();
 
         element.dropdown({
           delay: {
@@ -60,37 +59,6 @@
             },
           },
         });
-
-        if (autocompleteValue.split(',').filter(String).length > 0) {
-          const menuElement = element.find('div.menu');
-
-          menuElement.api({
-            on: 'now',
-            method: 'GET',
-            url: element.data('url'),
-            beforeSend(settings) {
-              const selectedMethod = $('input.input-shipping-method:checked');
-
-              /* eslint-disable-next-line no-param-reassign */
-              settings.urlData = {
-                providerCode: selectedMethod.data('pickup-point-provider'),
-                _csrf_token: selectedMethod.data('csrf-token'),
-              };
-
-              /* eslint-disable-next-line no-param-reassign */
-              settings.data[choiceValue] = autocompleteValue.split(',').filter(String);
-
-              return settings;
-            },
-            onSuccess(response) {
-              response.forEach((item) => {
-                menuElement.append((
-                  $(`<div class="item" data-value="${item[choiceValue]}">${item[choiceName]}</div>`)
-                ));
-              });
-            },
-          });
-        }
 
         window.setTimeout(() => {
           element.dropdown('set selected', element.find('input.autocomplete').val().split(',').filter(String));
