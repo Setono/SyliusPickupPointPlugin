9d5ca6ed485580fb6e0e0238bd44012cac8afeb4
Igor Mukhin <igor.mukhin@gmail.com>
2019-06-07 15:53:39 +0300

    Added: CachedProvider, it also solve faker provider issue

--- src/DependencyInjection/SetonoSyliusPickupPointExtension.php
+++ src/DependencyInjection/SetonoSyliusPickupPointExtension.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Setono\SyliusPickupPointPlugin\DependencyInjection;
 
 use Exception;
+use LogicException;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
@@ -16,6 +19,7 @@ final class SetonoSyliusPickupPointExtension extends Extension
      * {@inheritdoc}
      *
      * @throws Exception
+     * @throws LogicException
      */
     public function load(array $config, ContainerBuilder $container): void
     {
@@ -26,9 +30,24 @@ final class SetonoSyliusPickupPointExtension extends Extension
 
         $bundles = $container->getParameter('kernel.bundles');
 
+        $cacheEnabled = $config['cache']['enabled'];
+        if ($cacheEnabled) {
+            if (!interface_exists(AdapterInterface::class)) {
+                throw new LogicException('Using cache is only supported when symfony/cache is installed.');
+            }
+
+            if (null === $config['cache']['pool']) {
+                throw new LogicException('You should specify pool in order to use cache for pickup point providers.');
+            }
+
+            $container->setAlias(new Alias('setono_sylius_pickup_point.cache'), $config['cache']['pool']);
+        }
+
+        $container->setParameter('setono_sylius_pickup_point.cache.enabled', $cacheEnabled);
+
         if ($config['providers']['faker']) {
             if ('prod' === $container->getParameter('kernel.environment')) {
-                throw new Exception("You can't use faker provider in production environment.");
+                throw new LogicException("You can't use faker provider in production environment.");
             }
 
             $loader->load('services/providers/faker.xml');
@@ -36,7 +55,7 @@ final class SetonoSyliusPickupPointExtension extends Extension
 
         if ($config['providers']['dao']) {
             if (!isset($bundles['SetonoDAOBundle'])) {
-                throw new Exception('You should use SetonoDAOBundle or disable dao provider.');
+                throw new LogicException('You should use SetonoDAOBundle or disable dao provider.');
             }
 
             $loader->load('services/providers/dao.xml');
@@ -44,7 +63,7 @@ final class SetonoSyliusPickupPointExtension extends Extension
 
         if ($config['providers']['gls']) {
             if (!isset($bundles['SetonoGlsWebserviceBundle'])) {
-                throw new Exception('You should use SetonoGlsWebserviceBundle or disable gls provider.');
+                throw new LogicException('You should use SetonoGlsWebserviceBundle or disable gls provider.');
             }
 
             $loader->load('services/providers/gls.xml');
@@ -52,7 +71,7 @@ final class SetonoSyliusPickupPointExtension extends Extension
 
         if ($config['providers']['post_nord']) {
             if (!isset($bundles['SetonoPostNordBundle'])) {
-                throw new Exception('You should use SetonoPostNordBundle or disable post_nord provider.');
+                throw new LogicException('You should use SetonoPostNordBundle or disable post_nord provider.');
             }
 
             $loader->load('services/providers/post_nord.xml');
---
+++ src/Provider/CachedProvider.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusPickupPointPlugin\Provider;
+
+use Psr\Cache\CacheItemPoolInterface;
+use Setono\SyliusPickupPointPlugin\Model\PickupPointInterface;
+use Sylius\Component\Core\Model\AddressInterface;
+use Sylius\Component\Core\Model\OrderInterface;
+
+final class CachedProvider implements ProviderInterface
+{
+    /** @var CacheItemPoolInterface */
+    private $cacheItemPool;
+
+    /** @var ProviderInterface */
+    private $provider;
+
+    public function __construct(
+        CacheItemPoolInterface $cacheItemPool,
+        ProviderInterface $provider
+    ) {
+        $this->cacheItemPool = $cacheItemPool;
+        $this->provider = $provider;
+    }
+
+    public function findPickupPoints(OrderInterface $order): array
+    {
+        $orderCacheKey = $this->buildOrderCacheKey($order);
+        if (!$this->cacheItemPool->hasItem($orderCacheKey)) {
+            $pickupPoints = $this->provider->findPickupPoints($order);
+
+            $pickupPointsCacheItem = $this->cacheItemPool->getItem($orderCacheKey);
+            $pickupPointsCacheItem->set($pickupPoints);
+            $this->cacheItemPool->save($pickupPointsCacheItem);
+
+            // Store separate PickupPoints to retrieve at findOnePickupPointById
+            foreach ($pickupPoints as $pickupPoint) {
+                $pickupPointCacheKey = $this->buildPickupPointIdCacheKey($pickupPoint->getId());
+                $pickupPointCacheItem = $this->cacheItemPool->getItem($pickupPointCacheKey);
+                $pickupPointCacheItem->set($pickupPoint);
+                $this->cacheItemPool->save($pickupPointCacheItem);
+            }
+        }
+
+        return $this->cacheItemPool->getItem($orderCacheKey)->get();
+    }
+
+    public function findOnePickupPointById(string $id): ?PickupPointInterface
+    {
+        $pickupPointCacheKey = $this->buildPickupPointIdCacheKey($id);
+        if (!$this->cacheItemPool->hasItem($pickupPointCacheKey)) {
+            $pickupPointCacheItem = $this->cacheItemPool->getItem($pickupPointCacheKey);
+            $pickupPointCacheItem->set(
+                $this->provider->findOnePickupPointById($id)
+            );
+            $this->cacheItemPool->save($pickupPointCacheItem);
+        }
+
+        return $this->cacheItemPool->getItem($pickupPointCacheKey)->get();
+    }
+
+    public function getCode(): string
+    {
+        return $this->provider->getCode();
+    }
+
+    public function getName(): string
+    {
+        return $this->provider->getName();
+    }
+
+    private function buildOrderCacheKey(OrderInterface $order): string
+    {
+        $shippingAddress = $order->getShippingAddress();
+        if (!$shippingAddress instanceof AddressInterface) {
+            throw new \RuntimeException(sprintf(
+                'Shipping address was not found for order #%s',
+                $order->getNumber()
+            ));
+        }
+
+        // As far as DAO/Gls/PostNord using only these 3 fields to
+        // search for pickup points, we should build cache key based on them only
+        return sprintf(
+            '%s-%s-%s',
+            $shippingAddress->getCountryCode(),
+            $shippingAddress->getPostcode(),
+            $shippingAddress->getStreet()
+        );
+    }
+
+    private function buildPickupPointIdCacheKey(string $id): string
+    {
+        return sprintf(
+            '%s-%s',
+            $this->getCode(),
+            $id
+        );
+    }
+}
